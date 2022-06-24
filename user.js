7321de386ea6e6796e458b97be8b540f0410515f
Your Name <you@example.com>
2022-06-25 00:21:31 +0100

    JavaScript Introduction

---
+++ user.js
@@ -0,0 +1,8 @@
+alert("Hello World");
+
+let userName = "Name: Adediran Babatunde Adewumi";
+let height = "Height: 1.67ft";
+let country = "Country: Nigeria";
+
+alert(userName + "\n" + height + "\n" + country);
+document.write(userName + "<br>" + height + "<br>" + country);
